ab9045ecd54af6eb057f6e271fefaf6800a49558
cacevedod <cacevedod@devco.com.co>
2024-09-17 09:43:04 -0500

    dinamizando ensure

--- src/test/java/co/com/devco/airbnb/task/AbrirTodosServiciosDetalle.java
+++ src/test/java/co/com/devco/airbnb/task/AbrirTodosServiciosDetalle.java
@@ -7,23 +7,20 @@ import co.com.devco.airbnb.page.ResultadosEstadiaPage;
 import net.serenitybdd.core.pages.WebElementFacade;
 import net.serenitybdd.screenplay.Performable;
 import net.serenitybdd.screenplay.Task;
-import net.serenitybdd.screenplay.actions.*;
+import net.serenitybdd.screenplay.actions.Click;
+import net.serenitybdd.screenplay.actions.JavaScriptClick;
+import net.serenitybdd.screenplay.actions.Scroll;
+import net.serenitybdd.screenplay.actions.Switch;
 import net.serenitybdd.screenplay.ensure.Ensure;
-import net.serenitybdd.screenplay.waits.WaitUntil;
-import net.serenitybdd.screenplay.waits.WaitUntilTargetIsReady;
 
-import java.time.Duration;
 import java.util.List;
-
-import static net.serenitybdd.screenplay.matchers.WebElementStateMatchers.isVisible;
 
 
 public class AbrirTodosServiciosDetalle {
     public static Performable yValidarPara(Integer cantidadValidar, String criterioValidar, String nombreApartadoServicio) {
         return Task.where("ver el detalle del elemento'", actor -> {
             List<WebElementFacade> elementos = ResultadosEstadiaPage.ELEMENTOS_LISTA_RESULTADOS.resolveAllFor(actor).subList(0,cantidadValidar-1);
-            for (int i = 0; i < cantidadValidar; i++){
-                WebElementFacade elementWeb = elementos.get(i);
+            for (WebElementFacade elementWeb: elementos){
                 actor.attemptsTo(
                         Scroll.to(elementWeb),
                         JavaScriptClick.on(elementWeb),
